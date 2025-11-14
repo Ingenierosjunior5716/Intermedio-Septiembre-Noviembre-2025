9eb92ba02b17604e7b81518a19cbdc5be0714a17
Intermedio Ingenieros jr. <a01278171@tec.mx>
2025-11-14 13:37:24 -0600

    Update Ultrasonico.ino

--- Ultrasonico.ino
+++ Ultrasonico.ino
@@ -1,5 +1,4 @@
 void setup() {
-
 pinMode(10, OUTPUT); // TRIGGER
 pinMode(11, INPUT);  // ECHO
 Serial.begin(9600);  //Iniciamos el monitor serial
@@ -11,11 +10,11 @@ int trigger = 10;                         //Pin digital para el Trigger del sens
 int echo = 11;                            //Pin digital para el Echo del sensor
 float tiempo_de_espera,distancia;         //Variable decimal que usaremos más adelante
 
-digitalWrite (trigger,LOW);               // Apagamos el pin 8 durante 2 microsegundos
+digitalWrite (trigger,LOW);               // Apagamos el pin 10 durante 2 microsegundos
 delayMicroseconds(2);
 digitalWrite (trigger, HIGH);             // Ahora ponemos encendemos el pin 8 durante 10 microsegundos
 delayMicroseconds (10);                  
-digitalWrite (trigger, LOW);              // Apagamos pin 8
+digitalWrite (trigger, LOW);              // Apagamos pin 10
 tiempo_de_espera = pulseIn (echo,HIGH);   // pulseIn, recoge la señal del sonido que emite el trigger
 distancia =(tiempo_de_espera/2)/29.15;    // Formula para hallar la distancia
 Serial.print (distancia);                 // Imprimimos la distancia en el monitor serial
@@ -25,8 +24,5 @@ return distancia;                         // Si se llama a la función, toma el 
 
 void loop() {
 float d = ultra();   // La variable d ejecuta la función ultra() y luego almacena la distancia
-//Aquí escribe tu código
-
-
-
+  
 }
